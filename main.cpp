204b05f02b13256b72814e16b895d8ea42552ada
lauri <laurine.aujeau@isen-ouest.yncrea.fr>
2020-02-13 10:48:10 +0100

    oui

--- main.cpp
+++ main.cpp
@@ -1,14 +1,41 @@
 #include <iostream>
 #include <QApplication>
 #include <QPushButton>
-
-using namespace std;
+#include <QWidget>
+//THE BASE
+/*using namespace std;
 int main(int argc, char ** argv) {
     QApplication app (argc, argv);
     QPushButton button ("Hello world !");
     button.show();
     return app.exec();
-    int i=0;
+}*/
+
+using namespace std;
+int main(int argc, char ** argv) {
+    ///EXERCICE 1
+    /*QApplication app (argc, argv);
+    QPushButton button ;
+    button.setText("Hello world !");
+    button.setToolTip("Youpi !");
+    QFont qfont("Courrier", 16, 2);
+    button.setFont(qfont);
+    button.show();
+    return app.exec();*/
+    ///EXERCICE 2
+    /*QApplication app (argc, argv);
+    QPushButton button ("parent");
+    QPushButton button2 ("fils");
+    button.show();
+    button2.show();
+    return app.exec();*/
+    ///EXERCICE 3
+    QApplication app (argc, argv);
+    QWidget widget ;
+    widget.show();
+    return app.exec();
+
+    ///BONUS
     /*while(1){
         QApplication app (argc, argv);
         QPushButton button ("Hello world !");
